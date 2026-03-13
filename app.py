6e329b619b8efe52b9d72e09116e0c8b0e8adde7
Max27253773 <maxwade370@gmail.com>
2026-03-13 09:55:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ from PIL import Image, ImageDraw, ImageFont
 st.sidebar.markdown(
     """
     <div style="background: linear-gradient(90deg, #0026C7 0%, #FFFFFF 40%, #FFFFFF 60%, #C70000 100%); 
-                padding: 4px; border-radius: 5px; text-align: center; 
-                width: 60%; margin: 0 auto; box-shadow: 0px 2px 5px rgba(0,0,0,0.2);">
-            <h1 style="font-size: 10px; color: white; margin: 0; letter-spacing: 1px; text-transform: uppercase;">
+                padding: 2px; border-radius: 4px; text-align: center; 
+                width: 70%; margin: 0 auto; box-shadow: 0px 2px 4px rgba(0,0,0,0.2);">
+            <h1 style="font-size: 9px; color: white; margin: 0; letter-spacing: 1px; text-transform: uppercase;">
                 🌌 INTERSTELLAR GATE
             </h1>
         </div>
